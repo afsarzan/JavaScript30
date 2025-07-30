c85c9faf8835254c881fd67da18d61ca70647043
afsarzan <afsar_zan@yahoo.com>
2025-07-30 00:07:26 -0700

    Init: event listeners and vanilla js class manepulation

---
+++ 01 - JavaScript Drum Kit/main.js
@@ -0,0 +1,18 @@
+function removeTransition({ propertyName, target }) {
+  if (propertyName !== 'transform') return;
+  target.classList.remove('playing');
+}
+
+function playSound({ keyCode }) {
+  const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
+  const key = document.querySelector(`div[data-key="${keyCode}"]`);
+  if (!audio) return;
+
+  key.classList.add('playing');
+  audio.currentTime = 0;
+  audio.play();
+}
+
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
